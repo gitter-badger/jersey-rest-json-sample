59bca09864a65bc7dee42501f061b00394c8eae1
moony320 <moony320@gmail.com>
2015-08-03 22:05:01 +0800

    cors

--- search-api/src/main/java/com/ebay/chrysan/samples/searchapi/resource/entity/EntityOneResource.java
+++ search-api/src/main/java/com/ebay/chrysan/samples/searchapi/resource/entity/EntityOneResource.java
@@ -11,6 +11,7 @@ public class EntityOneResource extends Link {
 
     public EntityOneResource(UriInfo info, EntityOne entityOne) {
         super(info, entityOne);
+        put("id", entityOne.getId());
         put("name", entityOne.getName());
         put("created", entityOne.getCreated());
         put("entityTwo", new Link(getFullyQualifiedContextPath(info), entityOne.getEntityTwo()));
